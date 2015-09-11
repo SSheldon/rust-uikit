4f483fe0dbf5a313654086bb67058b20e78c74be
Steven Sheldon <steven@sasheldon.com>
2015-09-11 15:44:44 -0700

    Added stub example iOS app.

---
+++ ios-example/RustUIKitExample/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  RustUIKitExample
+//
+//  Created by Steven Sheldon on 9/11/15.
+//  Copyright © 2015 Steven Sheldon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, nil);
+  }
+}
