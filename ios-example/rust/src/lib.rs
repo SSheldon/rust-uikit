236b07808bfc16a7c7db2760e024d0ebb58062f4
Steven Sheldon <steven@sasheldon.com>
2015-09-12 14:15:01 -0500

    Moved a safe application_main wrapper to uikit.

--- ios-example/rust/src/lib.rs
+++ ios-example/rust/src/lib.rs
@@ -1,20 +1,10 @@
-extern crate objc_foundation;
 extern crate objc_uikit;
 extern crate objc_uikit_impl;
 
-use std::ptr;
-
-use objc_foundation::{INSObject, INSString, NSString};
-use objc_uikit::UIApplicationMain;
+use objc_uikit::application_main;
 use objc_uikit_impl::ApplicationDelegate;
 
 #[no_mangle]
 pub extern fn run_app() {
-    let class = ApplicationDelegate::class();
-    let class_name = NSString::from_str(class.name());
-    let class_name_ptr = &*class_name as *const _ as *mut _;
-
-    unsafe {
-        UIApplicationMain(0, ptr::null_mut(), ptr::null_mut(), class_name_ptr);
-    }
+    application_main::<ApplicationDelegate>();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,33 @@ extern crate objc;
 extern crate objc_id;
 extern crate objc_foundation;
 
+use std::env;
+use std::ffi::CString;
+use std::ptr;
+
 use libc::{c_char, c_int};
-use objc_foundation::{INSObject, NSString};
+use objc_foundation::{INSObject, INSString, NSString};
 
 #[link(name = "UIKit", kind = "framework")]
 extern {
-    pub fn UIApplicationMain(argc: c_int, argv: *mut *mut c_char,
+    fn UIApplicationMain(argc: c_int, argv: *mut *const c_char,
             principalClassName: *mut NSString,
             delegateClassName: *mut NSString) -> c_int;
 }
 
 pub trait IUIApplicationDelegate : INSObject { }
+
+pub fn application_main<T: IUIApplicationDelegate>() -> ! {
+    let args: Vec<_> = env::args().map(|s| CString::new(s).unwrap()).collect();
+    let mut arg_ptrs: Vec<_> = args.iter().map(|s| s.as_ptr()).collect();
+
+    let class = T::class();
+    let class_name = NSString::from_str(class.name());
+    let class_name_ptr = &*class_name as *const _ as *mut _;
+
+    unsafe {
+        UIApplicationMain(arg_ptrs.len() as c_int, arg_ptrs.as_mut_ptr(),
+            ptr::null_mut(), class_name_ptr);
+    }
+    unreachable!();
+}
