4092bd9a458c428b4283bfc91cc17a0e0fe39226
Steven Sheldon <steven@sasheldon.com>
2015-09-12 01:17:41 -0500

    Added an ApplicationDelegate struct.

--- impl/src/lib.rs
+++ impl/src/lib.rs
@@ -1,3 +1,26 @@
+extern crate objc;
+extern crate objc_foundation;
+
+use objc::Message;
+use objc::runtime::Class;
+use objc_foundation::INSObject;
+
 extern {
-    pub fn RustApplicationDelegateClass() -> *mut ();
+    fn RustApplicationDelegateClass() -> *mut Class;
+}
+
+pub struct ApplicationDelegate {
+    _private: (),
+}
+
+unsafe impl Message for ApplicationDelegate { }
+
+impl INSObject for ApplicationDelegate {
+    fn class() -> &'static Class {
+        unsafe {
+            let cls = RustApplicationDelegateClass();
+            assert!(!cls.is_null());
+            &*cls
+        }
+    }
 }
